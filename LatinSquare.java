999eed7c28a9523d7500477840c8446fd6ea014a
Ms Anastasia Sergeevna Zolova <azolova@cs-503-2-4.local>
2017-04-13 13:34:37 -0400

    exam submission

--- LatinSquare.java
+++ LatinSquare.java
@@ -1,95 +1,149 @@
 
 import java.io.*;
 
-public class LatinSquare 
-{
+public class LatinSquare {
 	// Matrix that will be used to hold the Latin square matrix data
 	Matrix M;
-	
+
 	/*
 	 * Read a file as a square matrix
 	 */
-	public LatinSquare(String filename) throws IOException
-	{
+	public LatinSquare(String filename) throws IOException {
 		// Open the file
-        BufferedReader reader = new BufferedReader(new FileReader(filename));
-        
-        // Read a row of text
-        String line;
-        int i = 0;
-        while ((line = reader.readLine()) != null) 
-        {
-        	// Split the text by tab
-            String[] parts = line.split("\t");
-            
-            // Create the matrix if we haven't from the number of parts
-            if (M == null)
-            {
-            	M = new Matrix(parts.length, parts.length);
-            }
-            
-            // Set the matrix from the line
-            for (int j=0; j<parts.length; j++)
-            {
-            	int value = Integer.parseInt(parts[j]);
-                M.setMatrixValue(i, j, value);
-            }
-            
-            // Increment the line number
-            i++;
-        }
-        
-        reader.close();
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+
+		// Read a row of text
+		String line;
+		int i = 0;
+		while ((line = reader.readLine()) != null) {
+			// Split the text by tab
+			String[] parts = line.split("\t");
+
+			// Create the matrix if we haven't from the number of parts
+			if (M == null) {
+				M = new Matrix(parts.length, parts.length);
+			}
+
+			// Set the matrix from the line
+			for (int j = 0; j < parts.length; j++) {
+				int value = Integer.parseInt(parts[j]);
+				M.setMatrixValue(i, j, value);
+			}
+
+			// Increment the line number
+			i++;
+		}
+
+		reader.close();
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
-	public String toString()
-	{
+	public String toString() {
 		return M.toString();
 	}
-	
+
 	/*
-	 * Find the value in the row. 
-	 * Return the column where found or -1 otherwise
+	 * Find the value in the row. Return the column where found or -1 otherwise
 	 */
-	public int findInRow(int i, int val)
-	{
+	public int findInRow(int i, int val) {
+
+		int[] matrix_size = M.getSize();
+		int m = matrix_size[0];
+		int n = matrix_size[1];
+
+		for (int j = 0; j < n; j++) {
+			if (val == M.getMatrixValue(i, j)) {
+
+				return j;
+			}
+		}
+
+		return -1;
+
+	}
+
+	/*
+	 * Find the value in the column Return the row where found or -1 otherwise
+	 */
+	public int findInColumn(int j, int val) {
+
+		int[] matrix_size = M.getSize();
+		int m = matrix_size[0];
+		int n = matrix_size[1];
+
+		for (int i = 0; i < m; i++) {
+
+			if (val == M.getMatrixValue(i, j)) {
+
+				return i;
+			}
+
+		}
 		return -1;
 	}
-	
-	/*
-	 * Find the value in the column
-	 * Return the row where found or -1 otherwise
-	 */
-	public int findInColumn(int j, int val)
-	{
-		return -1;
-	}
-	
+
 	/*
 	 * Check if the row is a valid for a Latin square
 	 */
-	public boolean isRowLatin(int row)
-	{
-		return false;
+	public boolean isRowLatin(int row) {
+		int[] matrix_size = M.getSize();
+		int m = matrix_size[0];
+		int n = matrix_size[1];
+
+		for (int i = 0; i < n; i++) {
+			if (this.findInRow(row, i + 1) == -1) {
+				return false;
+			}
+		}
+		return true;
 	}
-	
+
 	/*
 	 * Check if the column is valid for a Latin square
 	 */
-	public boolean isColumnLatin(int col)
-	{
-		return false;
+	public boolean isColumnLatin(int col) {
+		int[] matrix_size = M.getSize();
+		int m = matrix_size[0];
+		int n = matrix_size[1];
+
+		for (int j = 0; j < m; j++) {
+			if (this.findInColumn(col, j+1) == -1) {
+
+				return false;
+			}
+		}
+		return true;
 	}
-	
+
 	/*
 	 * Check if the matrix is a Latin square
 	 */
-	public boolean isLatin()
+	public boolean isLatin() {
+		int[] matrix_size = M.getSize();
+		int m = matrix_size[0];
+		int n = matrix_size[1];
+
+		for (int i = 0; i < n; i++) {
+			if (this.isRowLatin(i) == false) {
+				return false;
+			}
+
+		}
+		for (int j = 0; j < m; j++) {
+			if (this.isColumnLatin(j) == false) {
+				return false;
+			}
+
+		}
+
+		return true;
+	}
+	public int[] getSize()
 	{
-		return false;
+		return M.getSize();
 	}
 }
--- LatinSquaresApp.java
+++ LatinSquaresApp.java
@@ -1,23 +1,40 @@
 import java.io.IOException;
 
-public class LatinSquaresApp 
-{
-	public static void main(String[] args) throws IOException
-	{
+public class LatinSquaresApp {
+
+	public static void main(String[] args) throws IOException {
 		// The files that we will check are Latin squares
-		String[] files = {"Test1.txt", "Test2.txt", "Test3.txt", "Test4.txt"};
-		
-		for (String file : files)
-		{
+		String[] files = { "Test1.txt", "Test2.txt", "Test3.txt", "Test4.txt" };
+
+		for (String file : files) {
 			// Create a magic square
 			LatinSquare ms = new LatinSquare(file);
-			
+
 			// Print out the matrix
 			System.out.print(ms.toString());
-			
+
 			// Tell the user if is a magic square or not
+
+			int[] matrix_size = ms.getSize();
+			int m = matrix_size[0];
+			int n = matrix_size[1];
+
+			System.out.print("rows: ");
+			for (int j = 0; j < n; j++) {
+				System.out.print(ms.isRowLatin(j) + " ");
+
+			}
+			System.out.println("");
+			System.out.print("cols: ");
+			for (int i = 0; i < m; i++) {
+
+				System.out.print(ms.isColumnLatin(i) + " ");
+
+			}
+			System.out.println("");
 			System.out.println(ms.isLatin());
 			System.out.println("");
+
 		}
 	}
 }
